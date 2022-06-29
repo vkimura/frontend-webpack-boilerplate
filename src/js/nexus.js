c69910ef8325a6c986c7375d5b1ea35f5186d0e0
Victor Kimura <Victor.Kimura@campus-support.ca>
2022-06-29 16:32:32 -0700

    Updates to eslint config

---
+++ src/js/nexus.js
@@ -0,0 +1,9 @@
+import '../scss/nexus.scss';
+
+$('.gn-icon-menu').hover(function() {
+    $('.gn-menu-wrapper').toggleClass('gn-open-part');
+});
+
+$('.gn-menu-wrapper').hover(function() {
+    $(this).toggleClass('gn-open-all');
+});
